0e0d312694489150ec7d956356e358cfd40b4822
drmoreira <a21240321@alunos.isec.pt>
2016-12-24 15:21:00 +0000

    Check internet connection done! All about Google Sign In in the rigth place

--- PPLFinder/app/src/main/java/pt/isec/a21240321/pplfinder/LoginActivity.java
+++ PPLFinder/app/src/main/java/pt/isec/a21240321/pplfinder/LoginActivity.java
@@ -1,27 +1,142 @@
 package pt.isec.a21240321.pplfinder;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener{
+
+    private static final int RC_SIGN_IN = 10;
+    SignInButton mSignInButton;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        SignInButton teste = (SignInButton )findViewById(R.id.sign_in_button);
-        teste.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        this.checkNetworkConnection();
 
+        // <editor-fold defaultstate="collapsed" desc="Google Sign In">
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        // </editor-fold>
+
+        mSignInButton = (SignInButton )findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(new SignInListener());
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Google Sign In">
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+
+            //iv.setImageBitmap(getImageBitmap(acct.getPhotoUrl().toString()));
+            //iv.setImageURI(acct.getPhotoUrl());
+            Uri uri = acct.getPhotoUrl();
+
+
+            if(uri != null) {
+                //mStatusTextView.setText(uri.toString());
+                //new MainActivity.DownloadImageTask(mImageView).execute(uri.toString());
+            }
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else {
+            // Signed out, show unauthenticated UI.
+            //updateUI(false);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Internet Connection">
+    private void checkNetworkConnection(){
+        if(!hasNetworkConnection()) {
+            internetConnectionErrorDialog md = new internetConnectionErrorDialog();
+            md.show(getFragmentManager(), "TAG");
+        }
+    }
+
+    //Check if there is internet connection on android phone
+    private boolean hasNetworkConnection() {
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    class internetConnectionErrorDialog extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(R.string.error_connection_title)
+                    .setMessage(R.string.error_connection_message)
+                    .setIcon(R.drawable.ic_error)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            finish();
+                        }
+                    });
+            return builder.create();
+        }
+    }
+    // </editor-fold>
+
+    class SignInListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
     }
 }
--- PPLFinder/app/src/main/java/pt/isec/a21240321/pplfinder/MainActivity.java
+++ PPLFinder/app/src/main/java/pt/isec/a21240321/pplfinder/MainActivity.java
@@ -40,82 +40,24 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class MainActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
-
-    private static final int RC_SIGN_IN = 20;
-    private static final String TAG = "";
-    SignInButton mSignInButton;
-    GoogleApiClient mGoogleApiClient;
-    TextView  mStatusTextView;
-    Button mSignOutButton;
-    public ImageView mImageView;
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build();
-
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-        /*
-        mSignInButton = (SignInButton)findViewById(R.id.sign_in_button);
-        mSignInButton.setOnClickListener(this);
-
-
-        mSignOutButton = (Button)findViewById(R.id.button_sign_out);
-        mSignOutButton.setOnClickListener(this);
-
-        mStatusTextView = (TextView) findViewById(R.id.StatusTextView);
-
-        mImageView = (ImageView) findViewById(R.id.imageView);
-        */
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        getMenuInflater().inflate(R.menu.menu_messages, menu);
-        getMenuInflater().inflate(R.menu.menu_filters, menu);
         getMenuInflater().inflate(R.menu.menu_refresh, menu);
+        getMenuInflater().inflate(R.menu.menu_filters, menu);
+        getMenuInflater().inflate(R.menu.menu_messages, menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.sign_in_button:
-                    signIn();
-                    break;
-                    // ...
-            /*
-            case R.id.button_sign_out:
-                    signOut();
-                    break;
-            */
-        }
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
+    /*
     private void signOut() {
         Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
                 new ResultCallback<Status>() {
@@ -125,45 +67,7 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.On
                     }
                 });
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-
-            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
-            mStatusTextView.setText(acct.getDisplayName() + " - " + acct.getId());
-
-            //iv.setImageBitmap(getImageBitmap(acct.getPhotoUrl().toString()));
-            //iv.setImageURI(acct.getPhotoUrl());
-            Uri uri = acct.getPhotoUrl();
-
-
-            if(uri != null) {
-                mStatusTextView.setText(uri.toString());
-                new DownloadImageTask(mImageView).execute(uri.toString());
-            }
-
-
-
-        } else {
-            // Signed out, show unauthenticated UI.
-            //updateUI(false);
-        }
-    }
-
+    */
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         ImageView bmImage;
 
